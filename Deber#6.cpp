20ad849331b41e888f7b89a092c52ad8bb477d72
erickloolpez <erickloopeze@hotmail.com>
2023-01-29 21:39:48 -0500

    Main menu

---
+++ Deber#6.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string.h>
+
+void exerciseOne(){
+
+}
+
+void exerciseTwo(){
+    
+}
+
+
+int main(){
+    int op = 0; 
+
+    std::cout<<"Enter your option [ 1/2 ]:";
+    std::cin>>op;
+
+    if (op == 1)
+        exerciseOne();
+    else
+        exerciseTwo();
+
+    return 0;
+}
