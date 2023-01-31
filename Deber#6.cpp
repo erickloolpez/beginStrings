dc74777c2271576fc211bb764f670b8cceed3218
erickloolpez <erickloopeze@hotmail.com>
2023-01-31 11:36:02 -0500

    firstMethod

--- Deber#6.cpp
+++ Deber#6.cpp
@@ -1,25 +1,50 @@
 #include <iostream>
-#include <string.h>
+#include <string>
+using namespace std;
 
-void exerciseOne(){
+void exerciseOne() {
+  char phrase[20];
+  string aux = "aeiou";
+  int num[] = {0, 0, 0, 0, 0};
 
+  cout << "\n->Enter any phrase:";
+  cin.getline(phrase, 20, '\n');
+
+
+  for (int i = 0; i < 20; i++) {
+    if(phrase[i] == aux[0])
+      num[0] += 1;
+    else if(phrase[i] == aux[1])
+      num[1] += 1;
+    else if(phrase[i] == aux[2])
+      num[2] += 1;
+    else if(phrase[i] == aux[3])
+      num[3] += 1;
+    else if(phrase[i] == aux[4])
+      num[4] += 1;
+  }
+
+  for (int j = 0; j < 4; j++) {
+    cout << "\nThe numbers of vowels [" << aux[j] << "]:" << num[j];
+  }
+
+  cout << endl;
 }
 
-void exerciseTwo(){
-    
-}
+void exerciseTwo() {}
 
+int main() {
+  int op = 0;
+  char name[20];
 
-int main(){
-    int op = 0; 
+  cout << "Enter your option [ 1/2 ]:";
+  cin >> op;
+  std::cin.ignore();
 
-    std::cout<<"Enter your option [ 1/2 ]:";
-    std::cin>>op;
+  if (op == 1)
+    exerciseOne();
+  else
+    exerciseTwo();
 
-    if (op == 1)
-        exerciseOne();
-    else
-        exerciseTwo();
-
-    return 0;
+  return 0;
 }
